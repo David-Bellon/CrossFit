97ee074293baac11b9a2811d8e6b4013f8fc3954
David <David>
2022-02-12 11:27:38 +0100

    a

--- threads.py
+++ threads.py
@@ -44,6 +44,33 @@ ejercicios = {
     "Pesas": 3
 }
 
+lista_ejercicios = list(ejercicios.keys())
+
+def realizar_ejercicio(persona):
+    ejer = persona.aparato
+    print(persona.nombre, "Empieza a hacer", ejer, "a la hora", time.time())
+    repeticiones = ejercicios[ejer]
+    for i in range(repeticiones):
+        print(persona.nombre, "acaba", i + 1, "de", repeticiones, "repeticiones")
+    
+    print(persona.nombre, "ha acabado con el ejercicio de", ejer, "a la hora", time.time())
+    if lista_ejercicios.index(ejer) == 4:
+        persona.aparato = lista_ejercicios[0]
+    else:
+        persona.aparato = lista_ejercicios[lista_ejercicios.index(persona.aparato) + 1]
+    
+
+def dirigir_clase(lista_alumnos):
+    actividades = []
+    for alumno in lista_alumnos:
+        x = threading.Thread(target=realizar_ejercicio, args=(alumno,))
+        actividades.append(x)
+        x.start()
+
+
+
 #De aqui pa bajo codigo de ejecutar y tal, no funciones ni nada
 
 Personas_en_clase = setUp_integrantes(Inicio_clase)
+
+dirigir_clase(Personas_en_clase)
